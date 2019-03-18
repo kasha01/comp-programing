5044cbd204d622dfd09e2d6c4ef00e261fe7b8d9
kasha01 <karam.ny@gmail.com>
2019-03-18 17:46:26 -0500

    add gitignore and codeblocks related files

--- InterviewQ/amazon_easy/main.cpp
+++ InterviewQ/amazon_easy/main.cpp
@@ -16,8 +16,6 @@ void print(string prefix, string s){
     }
 }
 
-
-
 int main()
 {
 	print("","abc");
--- InterviewQ/code_jam/main.cpp
+++ InterviewQ/code_jam/main.cpp
@@ -3,29 +3,6 @@ using namespace std;
 typedef unsigned long long int uint64;
 
 
-int t;
-long long f, l;
-
-long long get(long long a) {
-	long long ba = a % 10;
-	a /= 10;
-	long long x = 0, v = 1, sum = 0;
-	while (a) {
-		x += a % 10 * v;
-		sum += a % 10;
-		a /= 10;
-		v *= 9;
-	}
-	long long re = x * 8;
-	for (long long i = 0; i <= ba; i++)
-		re += !!((sum + i) % 9);
-	// printf("x %lld ba %lld re %lld > %lld\n", x, ba, x * 8, re);
-	return re;
-}
-
 int main() {
-	long long l = 102;
-	long long f = 88;
-	cout << get(l) - get(f) + 1;
 	return 0;
 }
