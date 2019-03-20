f2174977e090988e320c08f5174b08f91c0af72d
kasha01 <karam.ny@gmail.com>
2019-03-19 22:22:44 -0500

    add c_sharp directory, containing geeks for geeks solution in C#

---
+++ GeeksForGeeks_csharp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GeeksForGeeks_csharp
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			count_of_strings_a_b_c c = new count_of_strings_a_b_c();
+			c.solve ();
+		}
+	}
+}
---
+++ GeeksForGeeks_csharp/google/easy/count_of_strings_a_b_c.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace GeeksForGeeks_csharp
+{
+	public class count_of_strings_a_b_c
+	{
+		public void solve()
+		{
+			// int put r
+			int r=3;
+
+			if (r == 1) {
+				Console.WriteLine ("Answer: " + 3);
+				return;
+			}
+
+			// 0b,0c
+			int s1=1;
+
+			// 0b,1c
+			double s2= r;
+
+			// 0b,2c
+			double s3=(r*(r-1))/2; 
+
+			// 1b,1c
+			double s4=r*(r-1);
+
+			// 1b,2c
+			double s5 = (r*(r-1)*(r-2))/2;
+
+			// 1b,0c
+			double s6 = r;
+
+			double ss = s1 + s2 + s3 + s4 + s5 + s6;
+			Console.WriteLine ("Answer: " + ss);
+		}
+	}
+}
