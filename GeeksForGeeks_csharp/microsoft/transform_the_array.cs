2679e502bf805be03b1dc8bfc3750d4c35216419
kasha-2 <karam.shamasyousif@gmail.com>
2019-11-20 22:18:01 -0600

    microsoft c# problem solutions

---
+++ GeeksForGeeks_csharp/microsoft/transform_the_array.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+// https://practice.geeksforgeeks.org/problems/transform-the-array/0
+namespace GeeksForGeeks_csharp
+{
+	public class transform_the_array
+	{
+		public static void Main ()
+		{
+			int[] arr = { 2, 4, 5, 0, 0, 5, 4, 8, 6, 0, 6, 8 };
+			solve (arr, 12);
+		}
+
+		static void solve(int[] arr, int n){
+			int prev = 0;
+			int i = 0;
+			int c = 0;
+
+			while (i < n) {
+				if (arr [i] != 0) {
+					prev = arr [i];
+					i++;
+					break;
+				}
+				i++;
+			}
+
+			while (i < n) {
+				int x = arr [i];
+
+				if (x == 0) {
+					i++;
+				} else if (x == prev) {
+					prev = prev * 2;
+					//Console.Write (prev * 2 + " ");
+					i++;
+				} else {
+					Console.Write (prev + " ");
+					prev = arr[i]; i++; c++;
+				}
+			}
+
+			if (prev > 0) {
+				Console.Write (prev + " ");
+				c++;
+			}
+
+			while (c < n) {
+				c++;
+				Console.Write (0 + " ");
+			}
+		}
+	}
+}
+
---
+++ GeeksForGeeks_csharp/microsoft/two_numbers_with_sum_closest_to_zero.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+// https://practice.geeksforgeeks.org/problems/two-numbers-with-sum-closest-to-zero/0
+namespace GeeksForGeeks_csharp
+{
+	public class two_numbers_with_sum_closest_to_zero
+	{
+
+		public static void test ()
+		{
+			int[] arr = { -8 ,-66 ,-60  };
+			solve (arr, 3);
+		}
+
+		static void solve(int[] arr, int n){
+			if (n == 2) {
+				Console.WriteLine (arr [0] + arr [1]);
+				return;
+			}				
+
+			Array.Sort (arr);
+
+			int sum = int.MaxValue;
+			int i = 0; int j = n - 1;
+
+			while (j-i > 1) {
+				if (arr[i] + arr[j] == 0){
+					Console.WriteLine (0);
+					return;
+				}
+
+				int s1 = arr[i] + arr [j - 1];
+				int s2 = arr[j] + arr [i + 1];
+
+				if (Math.Abs (s1) < Math.Abs (s2)) {
+					if (Math.Abs (s1) < Math.Abs(sum)) {
+						sum = s1;
+					}
+					j--;
+				} else if (Math.Abs (s1) > Math.Abs (s2)) {
+					if (Math.Abs (s2) < Math.Abs(sum)) {
+						sum = s2;
+					}
+					i++;
+				} else {
+					// s1 == s2
+					if (Math.Abs (s2) < Math.Abs(sum)) {
+						sum = s2;
+					}
+					i++;
+					j--;
+				}
+			}
+
+			Console.WriteLine(sum);
+		}
+	}
+}
+
