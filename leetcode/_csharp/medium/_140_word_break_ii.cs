910cfae7f5d790c36a21889db37fb865c2f28fc0
TheBigSpatula <kasha01@users.noreply.github.com>
2022-03-13 16:28:54 -0500

    Update _140_word_break_ii.cs

--- leetcode/_csharp/medium/_140_word_break_ii.cs
+++ leetcode/_csharp/medium/_140_word_break_ii.cs
@@ -8,8 +8,53 @@ namespace _csharp
 {
 	public class _140_word_break_ii
 	{
-		List<string> allResults = new List<string>();
+		
+		/// Word Break with memoization (faster)
+		HashSet<string> dict;
+		Dictionary<string,List<string>> memo;
+		public IList<string> WordBreak_Memo(string s, IList<string> wordDict) {
+		    dict = new HashSet<string>();
+		    memo = new Dictionary<string,List<string>>();
 
+		    foreach(string word in wordDict)
+			dict.Add(word);
+
+		    return rc(s).ToArray();
+		}
+
+		private List<string> rc(string s){
+		    int n = s.Length;
+		    if(n == 0)
+			return new List<string>(0);
+
+		    if(memo.ContainsKey(s)) return memo[s];
+
+		    var ret = new List<string>();
+		    if(dict.Contains(s))
+			ret.Add(s); // count for edge case if s exists in dict as a whole word (without segmentation).
+
+		    for(int l=1;l<n;++l){
+			string prefix = s.Substring(0,l);
+			if(dict.Contains(prefix)){
+			    // segment suffix
+			    string suffix = s.Substring(l,n-l);
+
+			    var ret_list = rc(suffix);
+			    foreach(string ss in ret_list){
+				ret.Add(prefix + " " + ss);
+			    }
+			}
+		    }
+
+		    memo.Add(s, ret);
+		    return ret;
+		}
+		
+		/// end word break with memoization
+		
+		
+		/// word break recurssive with no memoization
+		List<string> allResults = new List<string>();
 		public IList<string> WordBreak(string s, IList<string> wordDict) {
 			var set = new HashSet<string> ();
 			foreach (string word in wordDict) {
@@ -37,5 +82,7 @@ namespace _csharp
 				}
 			}
 		}
+		
+		/// end word break recurssive with no memoization
 	}
 }
